a043a8c81498987c4d950f1f0dd72e50e09fd9c7
Mohammed Alqadda <MohammedFAlqadda@gmail.com>
2026-03-02 20:08:46 +0300

    lab done + bonus

---
+++ lab.py
@@ -0,0 +1,18 @@
+from art import *
+from colorama import *
+
+def main():
+    print(art("random"))
+    phrase1 =text2art("BELIEVE AND ACHEIVE",font='block')
+    print(phrase1)
+    phrase2="HELLO"
+    tprint(phrase2,font="sub-zero",chr_ignore=True)
+
+    phrase3 = "Yin and Yang"
+    phrase3 = phrase3.split(" ")
+    phrase3 = [text2art(phrase3[0],font="soft",chr_ignore=True), text2art(phrase3[1],font="soft",chr_ignore=True), text2art(phrase3[2],font="soft",chr_ignore=True)]
+    print(f"{Back.BLACK + phrase3[0]}{Back.RESET+phrase3[1]}{Back.WHITE+phrase3[2]}")
+    print(Back.RESET)
+
+if __name__ == "__main__":
+    main()
