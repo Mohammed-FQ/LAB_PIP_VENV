b8f9712cd14ad65db8883efad43113f9af2dde51
Mohammed Alqadda <MohammedFAlqadda@gmail.com>
2026-03-02 20:12:57 +0300

    lab done + bonus

--- lab.py
+++ lab.py
@@ -8,10 +8,10 @@ def main():
     phrase2="HELLO"
     tprint(phrase2,font="sub-zero",chr_ignore=True)
 
-    phrase3 = "Yin and Yang"
+    phrase3 = "Yin Yang"
     phrase3 = phrase3.split(" ")
-    phrase3 = [text2art(phrase3[0],font="soft",chr_ignore=True), text2art(phrase3[1],font="soft",chr_ignore=True), text2art(phrase3[2],font="soft",chr_ignore=True)]
-    print(f"{Back.BLACK + phrase3[0]}{Back.WHITE+phrase3[2]}")
+    phrase3 = [text2art(phrase3[0],font="soft",chr_ignore=True), text2art(phrase3[1],font="soft",chr_ignore=True)]
+    print(f"{Back.BLACK + phrase3[0]}{Back.WHITE+phrase3[1]}")
     print(Back.RESET)
 
 if __name__ == "__main__":
